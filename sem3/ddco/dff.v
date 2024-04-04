b283cb314f3febd53fe6de16f21dfaadcf628d7b
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-04-04 23:32:27 +0530

    Create dff.v

---
+++ sem3/ddco/dff.v
@@ -0,0 +1,12 @@
+module dff(q,qbar,d,clk);
+	input d,clk;
+	output q,qbar;
+	wire not1_out;
+	wire nand1_out;
+	wire nand2_out;
+	not(not1_out,d);
+	nand(nand1_out,clk,d);
+	nand(nand2_out,clk,not1_out);
+	nand(q,qbar,nand1_out);
+	nand(qbar,q,nand2_out);
+endmodule
