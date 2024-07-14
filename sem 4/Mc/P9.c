25b30fb264b451f3604cbcc0f6073c09414c5b5a
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-14 22:38:10 +0530

    Create P9.c

---
+++ sem 4/Mc/P9.c
@@ -0,0 +1,20 @@
+//Simulate in pros in C for ARM microcontiller to demonstrate case conversion of characters from upper to lower case upper case and lower to
+
+#include <stdio.h>
+#include <lpc214x.h>
+
+int main () 
+{
+  char ch = 'c';
+  if(ch >= 'a' && ch < = 'z')
+  {
+    PINSEL0 = Ch;
+  }
+  else if (ch >= 'A' && ch< = 'Z')
+  {
+    PINSEL0 = Ch;
+  }
+  else
+    PINSEL0=ch;
+  return 0;
+}
