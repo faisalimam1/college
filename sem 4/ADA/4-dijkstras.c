836fa08483e669fc9185d4c8fef9d9dd18012d05
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-15 14:18:19 +0530

    Update 4-dijkstras.c

--- sem 4/ADA/4-dijkstras.c
+++ sem 4/ADA/4-dijkstras.c
@@ -1,45 +1,87 @@
+//Design and implement C/C++ Program to find shortest paths from a given vertex in a weighted connected graph to other vertices using Dijkstra's algorithm.
+
 #include<stdio.h>
 
-void dijkstra(int n,int s, int cost[][10]){
-    int i,count,u,v,min,dis[10],visited[10];
+int cost[10][10],n,dist[10];
+int minm(int m, int n)
+{
+	return((m<n)? m: n); 
+}
+void dijkstra(int source)
+{
+	int s[10] = {0} ;
+	int min, w = 0 ;
+	for(int i=0;i<n;i++)
+		dist[i]=cost[source][i]; 
+	dist[source] = 0; 
+	s[source] = 1; 
+	for(int i=0; i<n-1; i++) 
+	{
+		min = 999; 
+		for(int j = 0; j<n; j++) 
+		{ 
+			if((s[j] == 0) && (min>dist[j])) 
+			{
+				min = dist[j]; 
+				w = j;
+			} 
+		}
+		s[w] = 1 ;
+		for(int v=0;v<n;v++) 
+		{ 
+			if(s[v]==0 && cost[w][v]!= 999 )
+			{ 
+				dist[v]= minm(dist[v],dist[w]+cost[w][v]);
 
-    for (i=0;i<n;i++){
-        dis[i]=cost[s][i];
-        visited[i]=0;
-    }
-    dis[s]=0;
-    visited[s]=1;
-    for(count=0;count<n-1;count++){
-        min=999;
-        for(i=0;i<n;i++){
-            if(visited[i]==0 && dis[i]<min ){
-                min=dis[i];
-                u=i;
-            }
-        }
-        visited[u]=1;
+			}
+		}
+	}
+}
+int main() 
+{
+	int source;
+	printf("Enter the no.of vertices:"); 
+	scanf("%d",&n);
+	printf("Enter the cost matrix\n"); 
+	for(int i=0;i<n;i++) 
+	{
+		for(int j=0;j<n;j++)
+			scanf("%d",&cost[i][j]);
+	}
+	printf("Enter the source vertex:"); 
+	scanf("%d", &source);
+	dijkstra(source);
+	printf("the shortest distance is..."); 
+	for(int i=0; i<n; i++)
+		printf("Cost from %d to %d is %d\n",source,i,dist[i]);
 
-        for(v=0;v<n;v++){
-            if(visited[v]==0 && cost[u][v]!=999 && dis[v]>(dis[u]+cost[u][v]))
-            dis[v]=dis[u]+cost[u][v];
-        }
-    }
-
-    printf("The shortest path is\n");
-    for(v=0;v<n;v++)
-        printf("%d to %d is %d\n",s,v,dis[v]);
 }
 
+/* Sample Input and Output:
 
-void main(){
-    int n,i,j,s,cost[10][10];
-    printf("Enter the number of vertices: ");
-    scanf("%d",&n);
-    printf("Enter the cost matrix\n");
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            scanf("%d",&cost[i][j]);
-    printf("Enter the source vertex: ");
-    scanf("%d",&s);
-    dijkstra(n,s,cost);
-}
+Enter the no. of vertices:5
+
+Enter the cost matrix
+
+0 3 1 999 999
+
+3 0 7 5 1
+
+1 7 0 2 999
+
+999 5 2 0 7
+
+999 1 999 7 0
+
+Enter the source vertex:0
+
+the shortest distance is... Cost from 0 to 0 is O
+
+Cost from 0 to 1 is 3
+
+Cost from 0 to 2 is 1
+
+Cost from 0 to 3 is 3
+
+Cost from 0 to 4 is 4
+*/
