3a486791c637ab9ee1ac4248c9e43b5a04d9f3bf
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-15 23:29:43 +0530

    Update 1-krusals-algo.c

--- sem 4/ADA/1-krusals-algo.c
+++ sem 4/ADA/1-krusals-algo.c
@@ -1,40 +1,51 @@
+
 //prg 1
 
 #include<stdio.h>   
-
-void kruskal(int cost[][10], int n) {
-    int par[n], a, b, u, v, i, j, min, mincost = 0, ec = 0;
+int cost[10][10],n;
+void kruskal() 
+{
+    int par[n]; 
+	int a=0,b=0,u=0,v=0, i, j, min, mincost = 0, ne = 0;
 
     for (i = 0; i < n; i++)
         par[i] = -1;
-
-    while (ec < n - 1) {
+	printf("The min spanning tree edges are\n");
+    while (ne < n-1) 
+	{
         min = 999;
         for (i = 0; i < n; i++)
+		{
             for (j = 0; j < n; j++)
-                if (cost[i][j] < min) {
+			{
+                if (cost[i][j] < min) 
+				{
                     min = cost[i][j];
                     a = u = i;
                     b = v = j;
                 }
-        while (par[u] != -1)
+			}
+		}
+        while (par[u]!= -1)
             u = par[u];
-        while (par[v] != -1)
+        while (par[v]!= -1)
             v = par[v];
 
-        if (u != v) {
-            printf("edge from %d to  %d with cost %d is selected\n", a, b, min);
+        if (u != v) 
+		{
+            printf("from vertex %d to vertex %d and the cost=%d\n", a, b, min);
             mincost += min;
-            par[a] = b;
-            ec++;
+            par[v] = u;
+            ne++;
         }
         cost[a][b] = cost[b][a] = 999;
     }
-    printf("Minimum cost = %d\n", mincost);
+    printf("cost of MST = %d\n", mincost);
 }
 
-int main() {
-    int n, cost[10][10], i, j;
+int main() 
+{
+	int i,j;
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
 
@@ -43,6 +54,19 @@ int main() {
         for (j = 0; j < n; j++)
             scanf("%d", &cost[i][j]);
 
-    kruskal(cost, n);
+    kruskal();
     return 0;
 }
+/*
+Enter the number of vertices: 4
+Enter the cost matrix:
+999 2 3 1
+2 999 4 999
+3 4 999 5
+1 999 5 999
+The min spanning tree edges are
+from vertex 0 to vertex 3 and the cost=1
+from vertex 0 to vertex 1 and the cost=2
+from vertex 0 to vertex 2 and the cost=3
+cost of MST = 6
+*/
