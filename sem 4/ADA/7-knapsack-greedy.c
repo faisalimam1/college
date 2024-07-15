e3581dabd3015b4cbc405b035e79c914628b2da4
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-15 23:48:13 +0530

    Update 7-knapsack-greedy.c

--- sem 4/ADA/7-knapsack-greedy.c
+++ sem 4/ADA/7-knapsack-greedy.c
@@ -2,41 +2,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void greedy_ks(int n, int m, float p[], float w[]) {
+void greedy(int n, int m, float p[], float w[]) 
+{
     float profit = 0, max;
     int i, k, count;
-    for (count = 0; count < n; count++) {
+    for (count = 0; count < n; count++) 
+	{
         max = 0;
-        for (i = 0; i < n; i++) {
-            if (((p[i] / w[i]) > max)) {
+        for (i = 0; i < n; i++) 
+		{
+            if (((p[i] / w[i]) > max)) 
+			{
                 k = i;
                 max = p[i] / w[i];
             }
         }
-        if (w[k] <= m) {
-            printf("item %d with profit %f is selected\n", k, p[k]);
+        if (w[k] <= m) 
+		{
+            printf("item %d with profit %f is selected\n", (k+1), p[k]);
             profit += p[k];
             m -= w[k];
             p[k] = 0;
-        } else
+        } 
+		else
             break;
     }
     printf("Discrete knapsack profit= %f\n", profit);
-    float x = m / w[k];
-    profit += p[k] * x;
+    profit= profit + (m/w[k])*p[k];
     printf("Continuos knapsack profit= %f", profit);
 }
 
-void main() {
-    int n, m;
-    printf("Enter the no. of objects: ");
+void main() 
+{
+    int n, m,i;
+	float p[1000],w[1000];
+    printf("Enter the no. of items: ");
     scanf("%d", &n);
-    float* p, * w;
-
-    p = (float*)malloc(n * sizeof(float));
-    w = (float*)malloc(n * sizeof(float));
-
-    printf("Enter the capacity: ");
+    
+    printf("Enter the total capacity: ");
     scanf("%d", &m);
 
     printf("Enter the profit of %d items: ", n);
@@ -47,5 +50,17 @@ void main() {
     for (int i = 0;i < n;i++)
         scanf("%f", &w[i]);
 
-    greedy_ks(n, m, p, w);
+    greedy(n, m, p, w);
 }
+
+/*
+Enter the no. of items: 5
+Enter the total capacity: 100
+Enter the profit of 5 items: 20 30 60 40 60
+Enter the weight of 5 items: 10 20 30 40 50
+item 1 with profit 20.000000 is selected
+item 3 with profit 60.000000 is selected
+item 2 with profit 30.000000 is selected
+Discrete knapsack profit= 110.000000
+Continuos knapsack profit= 158.000000
+*/
