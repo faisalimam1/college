6c902c8f6257ab15acaccbbe4115ee128f4c304e
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-22 12:38:25 +0530

    Update 8.c

--- sem 4/ADA/8.c
+++ sem 4/ADA/8.c
@@ -1,41 +1,46 @@
 
 #include<stdio.h>
 
-int x[10], w[10], count = 0, n;
+int x[10], w[10], count=0, d,i,j;
 
-void sumofsubsets(int s, int k, int r) {
+void sumofsubsets(int s, int k, int r) 
+{
     x[k] = 1;
-    if (s + w[k] == n) {
+    if (s + w[k] == d) 
+	{
         printf("\nSubset %d:\n", ++count);
-        for (int i = 1; i <= k; i++) {
+        for (i = 0; i <= k; i++) 
+		{
             if (x[i] == 1)
                 printf("%d\t", w[i]);
         }
-    } else if (s + w[k] + w[k + 1] <= n)
+    } 
+	else if (s + w[k] + w[k + 1] <= d)
         sumofsubsets(s + w[k], k + 1, r - w[k]);
-    if ((s + r - w[k] >= n) && (s + w[k + 1] <= n)) {
+    if ((s + r - w[k] >= d) && (s + w[k + 1] <= d)) 
+	{
         x[k] = 0;
         sumofsubsets(s, k + 1, r - w[k]);
     }
 }
 
 int main() {
-    int sum = 0;
+    int sum = 0,n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
     printf("Enter the elements in ascending order:\n");
-    for (int i = 1; i <= n; i++) {
+    for (i = 0; i < n; i++) 
+	{
         scanf("%d", &w[i]);
-        sum += w[i];
+        sum = sum+w[i];
     }
-    if (sum < n) {
-        printf("No solution exists\n");
-        return 0;
-    }
+    
     printf("Enter the sum: ");
-    scanf("%d", &n);
-    sumofsubsets(0, 1, sum);
-    if (count == 0)
-        printf("No solution exists\n");
+    scanf("%d", &d);
+    if((sum<d)||(w[0]>d))
+    	printf("No subset possible\n");
+    else
+    	sumofsubsets(0,0,sum);
+    
     return 0;
 }
