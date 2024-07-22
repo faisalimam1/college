fb0d818972eec4d027b59d0333175d08ea670867
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-22 12:24:58 +0530

    Update 6-Knapsack-dp.c

--- sem 4/ADA/6-Knapsack-dp.c
+++ sem 4/ADA/6-Knapsack-dp.c
@@ -1,46 +1,58 @@
-//knapsack using dp
-#include <stdio.h>
-
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-
-void DPknapsack(int n, int m, int w[], int p[]) {
-    int i, j, V[n + 1][m + 1];
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= m; j++) {
-            if (i == 0 || j == 0)
-                V[i][j] = 0;
-            else if (j < w[i - 1])
-                V[i][j] = V[i - 1][j];
-            else
-                V[i][j] = max(V[i - 1][j], V[i - 1][j - w[i - 1]] + p[i - 1]);
-            printf("%d\t", V[i][j]);
-        }
-        printf("\n");
+ #include<stdio.h>
+ int n,m,p[10],w[10];
+ 
+ int max(int a,int b)
+ {
+	return (a>b?a:b);
+ }
+ void ksdp()
+ {
+ 	int v[11][11],i,j;
+ 	for(i=0;i<=n;i++)
+ 	{
+ 	
+ 		for(j=0;j<=m;j++)
+ 		{
+		 
+ 			if(i==0||j==0)
+ 				v[i][j]=0;
+ 			else if(j<w[i])
+ 				v[i][j]=v[i-1][j];
+ 			else
+ 				v[i][j]=max(v[i-1][j],p[i]+v[i-1][j-w[i]]);
+ 		}
     }
-    printf("Optimal profit: %d\n", V[n][m]);
-    printf("Items selected:\n");
-    while (m != 0) {
-        if (V[n][m] != V[n - 1][m]) {
-            printf("%d\t", n);
-            m -= w[n - 1];
-        }
-        n--;
-    }
-}
-
-void main() {
-    int n, m, i, w[10], p[10];
-    printf("Enter the number of items: ");
-    scanf("%d", &n);
-    printf("Enter the weights of the items: ");
-    for (i = 0; i < n; i++)
-        scanf("%d", &w[i]);
-    printf("Enter the profits of the items: ");
-    for (i = 0; i < n; i++)
-        scanf("%d", &p[i]);
-    printf("Enter the capacity of the knapsack: ");
-    scanf("%d", &m);
-    DPknapsack(n, m, w, p);
-}
+ 	printf("Hello\n");
+ 	for(i=0;i<=n;i++)
+ 	{
+ 		for(j=0;j<=m;j++)
+ 			printf("%d ",v[i][j]);
+ 		printf("\n");
+	}
+	printf("Items included are : ");
+	while(n>0)
+	{
+		if(v[n][m]!=v[n-1][m])
+		{
+			printf("%d ",n);
+			m=m-w[n];
+		}
+		n--;
+	}
+	
+ }
+ int main()
+ {
+ 	int i;
+ 	printf("Enter the no of items : ");
+ 	scanf("%d",&n);
+ 	printf("Enter the weight of n items : ");
+ 	for(i=1;i<=n;i++)
+ 		scanf("%d",&w[i]);
+ 	printf("Enter the prices of n items : ");
+ 	for(i=1;i<=n;i++)
+ 		scanf("%d",&p[i]);
+ 	printf("Enter the capacity of knapsack : ");
+ 	scanf("%d",&m);
+ 	ksdp();
+ }
