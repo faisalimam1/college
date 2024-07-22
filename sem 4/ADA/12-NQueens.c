a22fbc94559b9ebb6d22487bfd378c826ac0ba06
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-22 12:46:47 +0530

    Update 12-NQueens.c

--- sem 4/ADA/12-NQueens.c
+++ sem 4/ADA/12-NQueens.c
@@ -1,48 +1,59 @@
 
 #include<stdio.h>
 #include<math.h>
-
-int place(int x[], int k) {
+int i,j;
+int place(int x[], int k) 
+{
     int i;
-    for (i = 1; i < k; i++) {
+    for (i = 1; i < k; i++) 
+	{
         if (x[i] == x[k] || abs(x[i] - x[k]) == abs(i - k))
             return 0;
     }
     return 1;
 }
 
-int nqueen(int n) {
-    int x[10], count = 0, k = 1;
+int nqueen(int n) 
+{
+    int x[10], count = 0, k=1;
     x[k] = 0;
-    while (k != 0) {
+    while (k != 0) 
+	{
         x[k]++;
-        while (x[k] <= n && !place(x, k))
+        while ((x[k] <= n) && (!place(x, k)))
             x[k]++;
-        if (x[k] <= n) {
-            if (k == n) {
-                count++;
-                printf("\nSolution %d:\n", count);
-                for (int i = 1; i <= n; i++) {
-                    for (int j = 1; j <= n; j++) {
+        if (x[k] <= n) 
+		{
+            if (k == n) 
+			{
+                
+                printf("\nSolution %d:\n", ++count);
+                for (i = 1; i <= n; i++) 
+				{
+                    for (j = 1; j <= n; j++) 
+					{
                         printf("%c ", (j == x[i]) ? 'Q' : 'X');
-                    }
+                	}
                     printf("\n");
+             	   
                 }
-            } else {
-                k++;
+            } 
+			else 
+			{
+                ++k;
                 x[k] = 0;
             }
-        } else
+        } 
+		else
             k--;
     }
     return count;
 }
 
 int main() {
-    int n, count;
-    printf("Enter the number of queens: ");
+    int n;
+    printf("Enter the size of chessboard: ");
     scanf("%d", &n);
-    count = nqueen(n);
-    printf("\nTotal number of solutions: %d\n", count);
+    printf("\nTotal number of solutions: %d\n", nqueen(n));
     return 0;
 }
