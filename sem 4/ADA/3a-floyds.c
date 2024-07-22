b5e8dbf4a59fc8db93d5e4aca189582bebc8f6ea
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-22 12:57:00 +0530

    Update 3a-floyds.c

--- sem 4/ADA/3a-floyds.c
+++ sem 4/ADA/3a-floyds.c
@@ -2,7 +2,7 @@
 //prg 3a
 
 #include<stdio.h>
-
+int i,j,k;
 int min(int a, int b) 
 {
     return a < b ? a : b;
@@ -10,12 +10,12 @@ int min(int a, int b)
 
 void floyds(int d[][10], int n) 
 {
-    for (int k = 0; k < n; k++) 
+    for ( k = 0; k < n; k++) 
     {
-        printf("\nD(%d)\n", k);
-        for (int i = 0; i < n; i++) 
+       // printf("\nD(%d)\n", k);
+        for ( i = 0; i < n; i++) 
         {
-            for (int j = 0; j < n; j++) 
+            for ( j = 0; j < n; j++) 
             {
                 d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
                 printf("%d\t", d[i][j]);
@@ -31,11 +31,19 @@ int main()
     int n, d[10][10];
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the weights: \n");
-    for (int i = 0; i < n; i++) 
+    printf("Enter the cost matrix: \n");
+    for ( i = 0; i < n; i++) 
     {
-        for (int j = 0; j < n; j++)
+        for ( j = 0; j < n; j++)
             scanf("%d", &d[i][j]);
     }
     floyds(d, n);
+    printf("all pair shortest path\n");
+     for ( i = 0; i < n; i++) 
+    {
+        for ( j = 0; j < n; j++)
+            printf("%d ", d[i][j]);
+        printf("\n");
+    }
+    
 }
