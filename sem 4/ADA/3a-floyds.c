8632d349726538c8d001ecf2d8f45731b50aab54
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-07-23 12:18:37 +0530

    Update 3a-floyds.c

--- sem 4/ADA/3a-floyds.c
+++ sem 4/ADA/3a-floyds.c
@@ -31,7 +31,7 @@ int main()
     int n, d[10][10];
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the cost matrix: \n");
+    printf("Enter the cost matrix\n");
     for ( i = 0; i < n; i++) 
     {
         for ( j = 0; j < n; j++)
@@ -47,3 +47,15 @@ int main()
     }
     
 }
+/*
+Enter the number of vertices: 4
+Enter the cost matrix
+999 8 4 999
+999 999 1 999
+4 999 999 999
+999 2 9 999
+all pair shortest path
+8 8 4 999
+5 13 1 999
+4 12 8 999
+7 2 3 999
