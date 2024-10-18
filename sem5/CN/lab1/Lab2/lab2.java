c5801a342668ed5758282ac5d31475737f8ce0b6
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-10-18 09:35:33 +0530

    Update lab2.java

--- sem5/CN/lab1/Lab2/lab2.java
+++ sem5/CN/lab1/Lab2/lab2.java
@@ -1,5 +1,7 @@
+package crc;
+import java.util.Scanner;
 import java.io.*;
-class crc
+class Crc
 {
 public static void main(String a[]) throws IOException
 {
@@ -11,23 +13,27 @@ int[] app_message;
 int[] rem;
 int[] trans_message;
 int message_bits,gen_bits, total_bits;
+Scanner scanner = new Scanner(System.in);
 System.out.println("\n Enter number of bits in message : ");
-message_bits=Integer.parseInt(br.readLine());
+message_bits=scanner.nextInt();
 message=new int[message_bits];
 System.out.println("\n Enter message bits : ");
 for(int i=0; i<message_bits; i++)
-message[i]=Integer.parseInt(br.readLine());
+{
+ message[i]=scanner.nextInt();
+} 
 System.out.println("\n Enter number of bits in gen : ");
-gen_bits=Integer.parseInt(br.readLine());
+gen_bits=scanner.nextInt();
 gen=new int[gen_bits];
 System.out.println("\n Enter gen bits : ");
 for(int i=0; i<gen_bits; i++)
 {
-gen[i]=Integer.parseInt(br.readLine());
+gen[i]=scanner.nextInt();
 }
 total_bits=message_bits+gen_bits-1;
 app_message=new int[total_bits];
 rem=new int[total_bits];
+trans_message=new int[total_bits];
 for(int i=0;i<message.length;i++)
 {
 app_message[i]=message[i];
@@ -60,10 +66,8 @@ System.out.println("\n Transmitted message from the transmitter is : ");
 for(int i=0;i<trans_message.length;i++)
 {
 System.out.print(trans_message[i]);
-trans_message=new int[total_bits];
 }
-System.out.println("\n Enter received message of "+total_bits+" bits at receiver end 
-: ");
+System.out.println("\n Enter received message of "+total_bits+" bits at receiver end : \n");
 for(int i=0; i<trans_message.length; i++)
 {
 trans_message[i]=Integer.parseInt(br.readLine());
@@ -112,35 +116,3 @@ break;
 return rem;
 }
 }
-
-/*
-OUTPUT:Enter the number of bits in message:
-5
-Enter message bits:
-1
-0
-1
-1
-0
-Enter number of bits in gen:
-3
-Enter gen bits:
-1
-0
-1
-Message bits are:10110
-Generator bits are:101
-Appended message is:1011000
-Transmitted message from the trasmitter is:
-1011010
-Enter received message of 7 bits at receiver end:
-1
-0
-1
-1
-0
-1
-1
-Recieved message is : 1011011
-There is Error in the recieved message!!!
-*/
