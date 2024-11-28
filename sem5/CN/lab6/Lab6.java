35bd9259f5d0d72606e59fff6de54b34ac2cef1e
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-11-29 00:43:06 +0530

    Update Lab6.java

--- sem5/CN/lab6/Lab6.java
+++ sem5/CN/lab6/Lab6.java
@@ -1,1 +1,209 @@
+import java.util.Scanner;
 
+public class BellmanFord
+
+{
+
+ private int distances[];
+
+ private int numberofvertices;
+
+ public static final int MAX_VALUE = 999;
+
+ public BellmanFord(int numberofvertices)
+
+ {
+
+ this.numberofvertices = numberofvertices;
+
+ distances = new int[numberofvertices + 1];
+
+ }
+
+ public void BellmanFordEvaluation(int source, int destination,
+
+ int adjacencymatrix[][])
+
+ {
+
+ for (int node = 1; node <= numberofvertices; node++)
+
+ {
+
+ distances[node] = MAX_VALUE;
+
+ }
+
+ distances[source] = 0;
+
+ for (int node = 1; node <= numberofvertices - 1; node++)
+
+ {
+
+ for (int sourcenode = 1; sourcenode <= numberofvertices; sourcenode++)
+
+ {
+
+ for (int destinationnode = 1; destinationnode <= numberofvertices; 
+
+destinationnode++)
+
+ {
+
+ if (adjacencymatrix[sourcenode][destinationnode] != 
+
+MAX_VALUE)
+
+ {
+
+ if (distances[destinationnode] > distances[sourcenode]
+
+ + adjacencymatrix[sourcenode][destinationnode])
+
+ distances[destinationnode] = distances[sourcenode]
+
+ + adjacencymatrix[sourcenode][destinationnode];
+
+ }
+
+ }
+   }
+
+ }
+
+ for (int sourcenode = 1; sourcenode <= numberofvertices; sourcenode++)
+
+ {
+
+ for (int destinationnode = 1; destinationnode <= numberofvertices; 
+
+destinationnode++)
+
+ {
+
+ if (adjacencymatrix[sourcenode][destinationnode] != MAX_VALUE)
+
+ {
+
+ if (distances[destinationnode] > distances[sourcenode]
+
+ + adjacencymatrix[sourcenode][destinationnode])
+
+ System.out.println("The Graph contains negative egde cycle");
+
+ } }
+
+ }
+
+ for (int vertex = 1; vertex <= numberofvertices; vertex++)
+
+{
+
+ if (vertex == destination) 
+
+ System.out.println("distance of source " + source + " to “ + vertex + " is " + 
+
+distances[vertex]);
+
+ }
+
+ }
+
+ public static void main(String... arg)
+
+ {
+
+ int numberofvertices = 0;
+
+ int source, destination;
+
+ Scanner scanner = new Scanner(System.in);
+
+ System.out.println("Enter the number of vertices");
+
+ numberofvertices = scanner.nextInt();
+
+ int adjacencymatrix[][] = new int[numberofvertices + 1][numberofvertices 
+
++ 1];
+
+ System.out.println("Enter the adjacency matrix");
+
+ for (int sourcenode = 1; sourcenode <= numberofvertices; sourcenode++)
+
+ {
+
+ for (int destinationnode = 1; destinationnode <= numberofvertices; 
+
+destinationnode++)
+
+ {
+
+ adjacencymatrix[sourcenode][destinationnode] = scanner
+
+ .nextInt();
+
+ if (sourcenode == destinationnode)
+
+ {adjacencymatrix[sourcenode][destinationnode] = 0;
+
+ continue;
+
+ }
+
+ if (adjacencymatrix[sourcenode][destinationnode] == 0)
+
+ {
+
+ adjacencymatrix[sourcenode][destinationnode] = MAX_VALUE;
+
+ }
+
+ }
+
+ }
+
+ System.out.println("Enter the source vertex");
+
+ source = scanner.nextInt();
+
+ System.out.println("Enter the destination vertex: ");
+
+ destination = scanner.nextInt();
+
+ BellmanFord bellmanford = new BellmanFord(numberofvertices);
+
+ bellmanford.BellmanFordEvaluation(source, destination, 
+
+adjacencymatrix);
+
+ scanner.close();
+
+ }
+
+}
+
+/*
+Enter the number of vertices 5
+
+Enter the adjacency matrix
+
+0 999 6 3 999 
+
+3 0 999 999 999
+
+999 999 0 2 999
+
+999 1 1 0 999
+
+999 4 999 2 0
+
+Enter the source vertex
+
+5
+
+Enter the destination vertex
+
+1
+
+Distance of source 5 to 1 is 6 */
