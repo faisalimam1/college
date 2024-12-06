122099a23208df635e7d4db6ba89356136be13c1
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2024-12-06 09:14:12 +0530

    Update Lab8.java

--- sem5/CN/Lab8/Lab8.java
+++ sem5/CN/Lab8/Lab8.java
@@ -1,53 +1,54 @@
+import java.net.*;
 
-import java.net.*; 
-class datagram { 
-public static int serverPort = 666; 
-public static int clientPort = 999; 
-public static int buffer_size = 1024; 
-public static DatagramSocket ds; 
-public static byte buffer[] = new byte[buffer_size]; 
-public static void TheServer() throws Exception { 
-int pos=0; 
-while (true) { 
-int c = System.in.read(); 
-switch(c) { 
-case -1:
-System.out.println("Server Quits."); 
-return; 
-case '\r': 
-break; 
-case '\n':
-ds.send(new DatagramPacket(buffer,pos,InetAddress.getLocalHost(),clientPort)); 
-pos=0; 
-break; 
-default: 
-buffer[pos++] = (byte) c; 
-} 
-} 
-} 
-public static void TheClient() throws Exception { 
-while(true) { 
-DatagramPacket p = new DatagramPacket(buffer, buffer.length); 
-ds.receive(p); 
-System.out.println(new String(p.getData(), 0, p.getLength())); 
-} 
-}
-  public static void main(String args[]) throws Exception { 
+public class DatagramApp {
+    public static int serverPort = 6666;
+    public static int clientPort = 9999;
+    public static int buffer_size = 1024;
+    public static DatagramSocket ds;
+    public static byte buffer[] = new byte[buffer_size];
 
-if(args.length == 1) { 
+    public static void TheServer() throws Exception {
+        System.out.println("Server started. Type messages to send to the client:");
+        int pos = 0;
+        while (true) {
+            int c = System.in.read();
+            switch (c) {
+                case -1: // EOF
+                    System.out.println("Server Quits.");
+                    return;
+                case '\r': // Ignore carriage return
+                    break;
+                case '\n': // End of message
+                    ds.send(new DatagramPacket(buffer, pos, InetAddress.getLocalHost(), clientPort));
+                    pos = 0;
+                    break;
+                default:
+                    if (pos < buffer_size) {
+                        buffer[pos++] = (byte) c;
+                    } else {
+                        System.out.println("Buffer overflow. Message too long!");
+                        pos = 0;
+                    }
+            }
+        }
+    }
 
-ds = new DatagramSocket(serverPort); 
+    public static void TheClient() throws Exception {
+        System.out.println("Client started. Waiting for messages...");
+        while (true) {
+            DatagramPacket p = new DatagramPacket(buffer, buffer.length);
+            ds.receive(p);
+            System.out.println("Received: " + new String(p.getData(), 0, p.getLength()));
+        }
+    }
 
-TheServer(); 
-
-} else { 
-
-ds = new DatagramSocket(clientPort); 
-
-TheClient(); 
-
-} 
-
-} 
-
+    public static void main(String args[]) throws Exception {
+        if (args.length == 1) {
+            ds = new DatagramSocket(serverPort);
+            TheServer();
+        } else {
+            ds = new DatagramSocket(clientPort);
+            TheClient();
+        }
+    }
 }
