ef7e901c426eb02e4dade3ebd06ee686c77358a6
Faisal Imam <163755226+faisalimam1@users.noreply.github.com>
2025-03-23 23:23:43 +0530

    Update exp3.py

--- sem6/ML/exp3.py
+++ sem6/ML/exp3.py
@@ -4,18 +4,20 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 #load iris dataset that will be in dictionary-key value pair
 iris=datasets.load_iris()
+'''
 print(iris.keys())
 print(iris["data"])
 print(iris["target_names"])
 print(iris["target"])
 print(iris["DESCR"])
+'''
 df=pd.DataFrame(iris['data'],columns=iris['feature_names'])
-df
+#df
 from sklearn.preprocessing import StandardScaler
 scaler= StandardScaler()
 scaler.fit(df)
 scaled_data=scaler.transform(df)
-print(scaled_data)
+#print(scaled_data)
 #apply PCA by importing
 from sklearn.decomposition import PCA
 pca=PCA(n_components=4)
@@ -25,7 +27,7 @@ pca=PCA(n_components=2)
 pca.fit(scaled_data)
 print("Varience value: ",pca.explained_variance_ratio_)
 x_pca= pca.transform(scaled_data)
-print(x_pca)
+#print(x_pca)
 plt.figure(figsize=(12,10))
 y=iris.target
 target_names=iris.target_names
